eadf69413f505bdf618a6d7e9b4a2f6efbd9a595
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-24 19:58:06 +0545

    linking Task model to User model

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
