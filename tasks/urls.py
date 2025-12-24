2f05bbe851d1ed369aa62966afa596f503a600ec
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-24 22:28:25 +0545

    registering urls for login and register

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,7 @@ from .views import TaskListCreateView, TaskDetailView
 urlpatterns = [
     path('tasks/', TaskListCreateView.as_view()),
     path('tasks/<int:pk>/', TaskDetailView.as_view())
+    path("register/", RegisterAPIView.as_view())
+    path("login/", LoginAPIView.as_view())
+
 ]
