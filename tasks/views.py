3ebd3f39ea235c669d6a93d1363ede64f91653c4
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-24 20:37:24 +0545

    authentication with generic view

--- tasks/views.py
+++ tasks/views.py
@@ -5,12 +5,22 @@ from rest_framework.permissions import IsAuthenticated
 
 
 class TaskListCreateView(generics.ListCreateAPIView):
-    queryset = Task.objects.all()
+    # queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user).order_by('-created_at')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Task.objects.all()
+    # queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user).order_by('-created_at')
 
 
 
