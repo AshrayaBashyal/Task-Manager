a14bc377b7196564d55deb25eab38d41e643a50b
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-24 22:22:12 +0545

    adding user login serializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 from .models import Task
 from django.contrib.auth.models import User
@@ -21,3 +22,20 @@ class RegisterSerializer(serializers.ModelSerializer):
                 password=validated_data['password']
             )
             return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(
+            username=data["username"],
+            password=data["password"]
+        )
+
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+
+        data["user"] = user
+        return data
