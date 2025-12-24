5a591befbcb4a446e7723ef8e822c072f1cdaccd
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-24 22:10:50 +0545

    adding register serializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 from .models import Task
+from django.contrib.auth.models import User
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['id', 'title', 'description', 'completed', 'created_at', 'updated_at']
         read_only_fields = ['created_at', 'updated_at', 'id']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+        def create(self, validated_data):
+            user = User.objects.create_user(
+                username=validated_data['username'],
+                email=validated_data['email'],
+                password=validated_data['password']
+            )
+            return user
