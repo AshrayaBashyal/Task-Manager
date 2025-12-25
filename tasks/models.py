20a707028fced5be851854c06a4e2a0cbc5e3949
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-25 14:18:43 +0545

    fixing custom user error

--- tasks/models.py
+++ tasks/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
-
-# Create your models here.
 
 class User(AbstractUser):
-    email = models.EmailField(max_length=255, unique=True)
-    bio = models.TextField(max_length=500, blank=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
@@ -14,8 +13,13 @@ class User(AbstractUser):
     def __str__(self):
         return self.email
 
+
 class Task(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False)
