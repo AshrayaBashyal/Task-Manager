3d5bffc4470ea7949fbf70a9cd1fccacf23493f7
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2025-12-25 14:23:52 +0545

    fixing custom user errors

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import authenticate
 from rest_framework import serializers
 from .models import Task
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
